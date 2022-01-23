741255aa6935720e621d03662030e29c83161287
Matthew Chan <mchanjh1@gmail.com>
2022-01-23 22:38:53 +0800

    finished practical

--- app/src/main/java/com/example/imageplayer/MainActivity.java
+++ app/src/main/java/com/example/imageplayer/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.imageplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public void onClick(View v) {
+        Intent i ;
+        switch (v.getId()) {
+            case R.id.btnImage:
+                i = new Intent(this, ImageActivity.class);
+                startActivity(i);
+                break;
+            case R.id.btnPlayer:
+                i = new Intent(this, MediaPlayerActivity.class);
+                startActivity(i);
+                break;
+        }
+    }
+
 }
